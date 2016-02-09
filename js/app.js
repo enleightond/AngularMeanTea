d458025d645ea622e9f074a4cad277a29b870557
Edward Tierra <edrichtierra@gmail.com>
2016-02-09 14:09:50 -0800

    more copy pasting

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('teaStore', ['ngRoute']);
 
 app.directive('singleTea', function() {
 	return {
-		templateUrl: '/templates/teaTemplate.html',
+		templateUrl: './templates/teaTemplate.html',
 		link: function(scope,element, attributes){
 		}
 	};
@@ -11,10 +11,10 @@ app.directive('singleTea', function() {
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: "partials/order.html",
+		templateUrl: "./partials/order.html",
 		controller: "Selection"
 	}).when('/checkout', {
-		templateUrl: "partials/checkout.html",
+		templateUrl: "./partials/checkout.html",
 		controller: "Checkout"
 	});
 })
